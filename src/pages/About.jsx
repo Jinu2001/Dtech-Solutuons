1668e152d03da8796598edca9cdb9a33e9d7e95a
K Amith Malinga <malingaamith1@gmail.com>
2024-02-27 08:31:55 +0530

    fixed footer alignments

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,19 +3,19 @@ import './About.css';
 
 const About = () => {
     return (
-        
-            <section className="description" style={{display:'flex' , flexDirection:'row'}}>
-                <img src='public/images/6733427.jpg' className="image2" alt="image2"/>
-                <p>
-                    <div className="about-us">
-                        <header className="header">
-                            <h1>About us</h1>
-                        </header>
-                     </div>
-                    Welcome to DTEC, your premier destination for online courses facilitated by esteemed tutors. Established in 2024, our mission at DTEC is to provide high-quality education to students, fostering well-rounded individuals poised to become knowledgeable citizens and valuable employees.
-                </p>
-            </section>
-        
+
+        <section className="description" style={{ display: 'flex', flexDirection: 'row' }}>
+            <img src='public/images/6733427.jpg' className="image2" alt="image2" />
+            <p>
+                <div className="about-us">
+                    <header className="header">
+                        <h1>About us</h1>
+                    </header>
+                </div>
+                Welcome to DTEC, your premier destination for online courses facilitated by esteemed tutors. Established in 2024, our mission at DTEC is to provide high-quality education to students, fostering well-rounded individuals poised to become knowledgeable citizens and valuable employees.
+            </p>
+        </section>
+
     );
 };
 
--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -31,23 +31,23 @@ const Footer = () => {
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
             <Box display="flex" alignItems="center" mb={1}>
-              <BusinessIcon sx={{ mr: 1, fontSize: '2.5rem', color: 'white' }} />
+              {/* <BusinessIcon sx={{ mr: 1, fontSize: '2.5rem', color: 'white' }} /> */}
               <Typography variant="h6" sx={{ color: 'white' }}>
                 DTEC
               </Typography>
             </Box>
-            <Typography variant="body2" sx={{ mb: 1, color: 'white' }}>
+            <Typography variant="body2" sx={{ mb: 1, color: '#EAECF0' }}>
               "Empowering innovation"
             </Typography>
             <Box display="flex" alignItems="center" mb={1}>
-              <LocationOnIcon sx={{ mr: 1, color: 'white' }} />
-              <Typography variant="body2" sx={{ color: 'white' }}>
+              <LocationOnIcon sx={{ width: '20px', height: '20px', mr: 1, color: '#EAECF0' }} />
+              <Typography variant="body2" sx={{ color: '#EAECF0' }}>
                 1234 Tech Road, Silicon Valley, CA
               </Typography>
             </Box>
             <Box display="flex" alignItems="center" mb={1}>
-              <EmailIcon sx={{ mr: 1, color: 'white' }} />
-              <Typography variant="body2" sx={{ color: 'white' }}>
+              <EmailIcon sx={{ width: '16px', height: '16px' , mr: 1, color: '#EAECF0' }} />
+              <Typography variant="body2" sx={{ color: '#EAECF0' }}>
                 contact@dtec.com
               </Typography>
             </Box>
@@ -61,22 +61,23 @@ const Footer = () => {
           display: 'flex',
           gap: '16px',
           p: '8px',
+          marginRight:"160px"
         }}>
           <PulsingIconButton color="inherit" href="https://twitter.com" target="_blank">
-            <TwitterIcon sx={{ color: '#1DA1F2' }} />
+            <TwitterIcon sx={{ color: '#98A2B3' }} />
           </PulsingIconButton>
           <PulsingIconButton color="inherit" href="https://linkedin.com" target="_blank">
-            <LinkedInIcon sx={{ color: '#0A66C2' }} />
+            <LinkedInIcon sx={{ color: '#98A2B3' }} />
           </PulsingIconButton>
           <PulsingIconButton color="inherit" href="https://facebook.com" target="_blank">
-            <FacebookIcon sx={{ color: '#4267B2' }} />
+            <FacebookIcon sx={{ color: '#98A2B3' }} />
           </PulsingIconButton>
           <PulsingIconButton color="inherit" href="https://youtube.com" target="_blank">
-            <YouTubeIcon sx={{ color: '#FF0000' }} />
+            <YouTubeIcon sx={{ color: '#98A2B3' }} />
           </PulsingIconButton>
         </Box>
       </Container>
-      <Typography variant="body2" color="inherit" sx={{ textAlign: 'center', mt: 3 }}>
+      <Typography variant="body2" color="#98A2B3" sx={{marginLeft:"160px" ,mt: 3, fontSize: "12px" }}>
         © 2024 DTEC. All rights reserved.
       </Typography>
     </Box>
