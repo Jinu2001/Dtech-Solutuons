434c2b4c28d4bccf4bc9f9ca7aa9869ea1de93ca
jinu <hasarijithmi@gmail.com>
2024-03-02 00:33:26 +0530

    updated responsive navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,8 +10,6 @@ const NavBar = () => {
   const location = useLocation();
   const [drawerOpen, setDrawerOpen] = useState(false);
 
-  // Log the isMobile state to the console for debugging
-  console.log('isMobile:', isMobile);
 
   const handleDrawerToggle = () => {
     setDrawerOpen(!drawerOpen);
@@ -36,13 +34,13 @@ const NavBar = () => {
   return (
     <AppBar position="fixed" sx={{ backgroundColor: 'white', boxShadow: 'none' }}>
       <Toolbar>
-        <img src='./assets/logo.png' alt="Logo" style={{ width: '50px', height: '70px', marginRight: '10px', marginTop: '5px' }} />
+        <img src='/images/logo.png' alt="Logo" style={{ width: '50px', height: '70px', marginRight: '10px', marginTop: '5px' }} />
         <Typography variant="h6" sx={{ flexGrow: 1, color: 'black' }}>
           DTEC
         </Typography>
         {isMobile ? (
           <>
-            <IconButton color="inherit" onClick={handleDrawerToggle} sx={{ display: 'block', color: 'black' }}>
+            <IconButton color="inherit" onClick={handleDrawerToggle} sx={{ display: 'block', color: '#1A87C9' }}>
              <MenuIcon />
              </IconButton>
             <Drawer
@@ -61,11 +59,11 @@ const NavBar = () => {
           </>
         ) : (
           <div style={{ display: 'flex', alignItems: 'center' }}>
-            <Button onClick={() => handleNavigation('home', '/')}>Home</Button>
-            <Button onClick={() => handleNavigation('about', '/')}>About</Button>
-            <Button onClick={() => handleNavigation('services', '/')}>Services</Button>
-            <Button onClick={() => handleNavigation('courses', '/')}>Courses</Button>
-            <Button style={{ backgroundColor: '#4883FF', color: 'white', marginLeft: '10px' }} onClick={() => handleNavigation('register', '/')}>Register</Button>
+            <Button onClick={() => handleNavigation('home', '/')} style={{color: 'black'}}>Home</Button>
+            <Button onClick={() => handleNavigation('about', '/')}style={{color: 'black'}}>About</Button>
+            <Button onClick={() => handleNavigation('services', '/')}style={{color: 'black'}}>Services</Button>
+            <Button onClick={() => handleNavigation('courses', '/')}style={{color: 'black'}}>Courses</Button>
+            <Button style={{backgroundColor:'#1A87C9',color: 'white', marginLeft: '10px'}} onClick={() => handleNavigation('register', '/')}>Register</Button>
           </div>
         )}
       </Toolbar>
