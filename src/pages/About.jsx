de60e1176ec64004f2231d8d50639e5437108597
K Amith Malinga <malingaamith1@gmail.com>
2024-03-02 12:09:33 +0530

    About section redesigned

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import './About.css';
+import { Grid } from '@mui/material';
 
 const About = () => {
-    return (
-
-        <section className="description" style={{ display: 'flex', flexDirection: 'row' }}>
-            <img src='public/images/6733427.jpg' className="image2" alt="image2" />
-            <p>
-                <div className="about-us">
-                    <header className="header">
-                        <h1>About us</h1>
-                    </header>
-                </div>
-                Welcome to DTEC, your premier destination for online courses facilitated by esteemed tutors. Established in 2024, our mission at DTEC is to provide high-quality education to students, fostering well-rounded individuals poised to become knowledgeable citizens and valuable employees.
+  return (
+    <section className="description1">
+      <Grid item xs={12} sm={6} md={3}>
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <div className='emptybox'></div>
+          <div className="about-us">
+            <header className="header1">
+              <div>Who We Are</div>
+            </header>
+            <p className='p1'>
+              Welcome to DTEC, your premier destination for online courses facilitated by esteemed tutors. Established in 2024, our mission at DTEC is to provide high-quality education to students, fostering well-rounded individuals poised to become knowledgeable citizens and valuable employees.
             </p>
-        </section>
-
-    );
+          </div>
+        </div>
+      </Grid>
+    </section>
+  );
 };
 
 export default About;
